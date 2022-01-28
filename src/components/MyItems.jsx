b4775c9f1386959b98fa7c8484330fddcbae39cb
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-28 14:05:55 +0100

    getting data

--- src/components/MyItems.jsx
+++ src/components/MyItems.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import SingleItem from "./SingleItem";
 import product from "../Data/product.json";
+import { Col, Row } from "react-bootstrap";
 
 function MyItems() {
   return (
-    <div className="d-flex justify-content-center">
-      <SingleItem />
+    <div className="my-5 d-flex justify-content-center">
+      <Row>
+        {product.map(post => (
+          <Col md={4}>
+            <SingleItem key={post.title} product={post} />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
 
-function SingleItem() {
+function SingleItem({ product }) {
+  const { name, imageUrl, category, price, description } = product;
   return (
-    <div className="my-5 d-flex justify-content-center">
+    <div className=" d-flex justify-content-center">
       <Row>
         <Col>
           <Card className="mx-2 mb-4" style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://images.unsplash.com/photo-1643306633346-30cf2efcab78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2075&q=80"
-            />
+            <Card.Img variant="top" src={imageUrl} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>{name}</Card.Title>
+              <Card.Text>{description}</Card.Text>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                {category} {price}
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
         </Col>
